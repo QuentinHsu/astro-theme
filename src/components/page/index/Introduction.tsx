c31c98ae22ab600fb0e5af16a5f8ae2a45625f7a
QuentinHsu <xuquentinyang@gmail.com>
2024-07-10 07:47:14 +0800

    refactor: update Introduction component with additional information

--- src/components/page/index/Introduction.tsx
+++ src/components/page/index/Introduction.tsx
@@ -29,7 +29,7 @@ export default function Introduction(): React.ReactElement {
         🕹️ I am primarily a web front-end developer, with a tech stack mainly focused on Vue. Of course, I also enjoy working with React. I am particularly fascinated by TypeScript, although I am still learning and practicing it. When it comes to code, I prefer explicit and controllable approaches, and dislike implicit and uncontrollable ones. I enjoy experimenting with new things and sharing my findings.
       </p>
       <p className={styleParagraph}>
-        📍 Currently residing in Chengdu, Sichuan, and planning to move to other cities in the future, such as Guangzhou or Shenzhen, in order to seek better career prospects.
+        📍 Currently residing in Chengdu, Sichuan, and planning to move to other cities in the future, such as Guangzhou or Shenzhen, in order to seek better career prospects. Also, open to remote job opportunities.
       </p>
     </>
   )
