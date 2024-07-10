34eebdac0bb992045d1c96c351a65eee7cff1d36
QuentinHsu <xuquentinyang@gmail.com>
2024-07-10 18:01:22 +0800

    refactor: add ListOfArticles component to display a list of blog posts

---
+++ src/components/page/post/ListOfArticles.tsx
@@ -0,0 +1,54 @@
+// src/components/PostList.tsx
+import React from 'react'
+import dayjs from 'dayjs'
+
+interface Post {
+  id: string
+  slug: string
+  body: string
+  collection: string
+  data: {
+    title: string
+    description: string
+    pubDate: Date
+    updatedDate?: Date
+    heroImage?: string
+  }
+}
+
+interface PostListProps {
+  posts: Post[]
+}
+
+const PostList: React.FC<PostListProps> = ({ posts }) => {
+  const postsByYear = posts.reduce<Map<number, Post[]>>((acc, post) => {
+    const year = dayjs(post.data.pubDate).year()
+    if (!acc.has(year)) {
+      acc.set(year, [])
+    }
+    acc.get(year)!.push(post)
+    return acc
+  }, new Map<number, Post[]>())
+
+  return (
+    <ul>
+      {Array.from(postsByYear).map(([year, posts]) => (
+        <li key={year}>
+          <h2 className="text-2xl font-bold">{year}</h2>
+          <ul className="ml-4">
+            {posts.map(post => (
+              <li key={post.slug}>
+                <a href={`/blog/${post.slug}/`} className="flex items-center justify-between">
+                  <h4 className="md:text-xl">{post.data.title}</h4>
+                  <p className="date m-2">{dayjs(post.data.pubDate).format('MM-DD')}</p>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default PostList
