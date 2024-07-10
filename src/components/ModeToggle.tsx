57d636b799d6f6f0b006034e86e2dcd67ecbe53f
QuentinHsu <xuquentinyang@gmail.com>
2024-07-10 22:22:29 +0800

    refactor: update ModeToggle component to support three themes and improve icon toggling

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,30 +1,49 @@
 import * as React from 'react'
-import { Moon, Sun } from 'lucide-react'
-
+import { Moon, Sun, SunMoon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 
+enum Theme {
+  Light = 'theme-light',
+  Dark = 'dark',
+  System = 'system',
+}
+
 export function ModeToggle() {
-  const [theme, setThemeState] = React.useState<
-    'theme-light' | 'dark' | 'system'
-  >('theme-light')
-
+  const [theme, setThemeState] = React.useState<Theme>(Theme.Light)
   React.useEffect(() => {
-    const isDarkMode = document.documentElement.classList.contains('dark')
-    setThemeState(isDarkMode ? 'dark' : 'theme-light')
+    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+    setThemeState(isDarkMode ? Theme.Dark : Theme.Light)
   }, [])
-
   React.useEffect(() => {
-    const isDark
-      = theme === 'dark'
-      || (theme === 'system'
-      && window.matchMedia('(prefers-color-scheme: dark)').matches)
-    document.documentElement.classList[isDark ? 'add' : 'remove']('dark')
+    const isDark = theme === Theme.Dark || (theme === Theme.System && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    document.documentElement.classList.toggle('dark', isDark)
   }, [theme])
 
+  const toggleTheme = React.useCallback(() => {
+    setThemeState((prevTheme) => {
+      switch (prevTheme) {
+        case Theme.Light:
+          return Theme.Dark
+        case Theme.Dark:
+          return Theme.System
+        case Theme.System:
+          return Theme.Light
+        default:
+          return Theme.Dark
+      }
+    })
+  }, [])
+
+  const defaultIconClass = 'h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all'
+  const sunIconClass = theme === Theme.Light ? defaultIconClass : 'hidden'
+  const moonIconClass = theme === Theme.Dark ? defaultIconClass : 'hidden'
+  const sunMoonIconClass = theme === Theme.System ? defaultIconClass : 'hidden'
+
   return (
-    <Button variant="ghost" size="icon">
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" onClick={() => setThemeState('dark')} />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" onClick={() => setThemeState('theme-light')} />
+    <Button variant="ghost" size="icon" onClick={toggleTheme}>
+      <Sun className={sunIconClass} />
+      <Moon className={moonIconClass} />
+      <SunMoon className={sunMoonIconClass} />
     </Button>
   )
 }
