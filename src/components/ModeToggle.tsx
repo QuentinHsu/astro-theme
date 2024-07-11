2ff5b6517b79ebd2b95d2a6c8140bdcdda60374e
QuentinHsu <xuquentinyang@gmail.com>
2024-07-11 16:08:34 +0800

    refactor: Improve ModeToggle component by simplifying and optimizing theme handling

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,49 +1,54 @@
 import * as React from 'react'
-import { Moon, Sun, SunMoon } from 'lucide-react'
+import { Moon, Sun } from 'lucide-react'
+import { classnames, height, transitionProperty, width } from 'tailwindcss-classnames'
 import { Button } from '@/components/ui/button'
 
 enum Theme {
-  Light = 'theme-light',
+  Light = 'light',
   Dark = 'dark',
-  System = 'system',
 }
 
 export function ModeToggle() {
-  const [theme, setThemeState] = React.useState<Theme>(Theme.Light)
+  const styleIcon = classnames(
+    width('w-5'),
+    height('h-5'),
+    transitionProperty('transition-all'),
+  )
+  const [theme, setThemeState] = React.useState<Theme>(Theme.Dark)
+
   React.useEffect(() => {
-    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
-    setThemeState(isDarkMode ? Theme.Dark : Theme.Light)
+    const storedTheme = localStorage.getItem('theme') as Theme
+    if (storedTheme) {
+      setThemeState(storedTheme)
+    }
+    else {
+      const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+      setThemeState(isDarkMode ? Theme.Dark : Theme.Light)
+    }
   }, [])
+
   React.useEffect(() => {
-    const isDark = theme === Theme.Dark || (theme === Theme.System && window.matchMedia('(prefers-color-scheme: dark)').matches)
-    document.documentElement.classList.toggle('dark', isDark)
+    localStorage.setItem('theme', theme)
+    document.documentElement.classList.toggle('dark', theme === Theme.Dark)
   }, [theme])
 
   const toggleTheme = React.useCallback(() => {
     setThemeState((prevTheme) => {
-      switch (prevTheme) {
-        case Theme.Light:
-          return Theme.Dark
-        case Theme.Dark:
-          return Theme.System
-        case Theme.System:
-          return Theme.Light
-        default:
-          return Theme.Dark
-      }
+      const newTheme = prevTheme === Theme.Light ? Theme.Dark : Theme.Light
+      localStorage.setItem('theme', newTheme)
+      return newTheme
     })
   }, [])
 
-  const defaultIconClass = 'h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all'
-  const sunIconClass = theme === Theme.Light ? defaultIconClass : 'hidden'
-  const moonIconClass = theme === Theme.Dark ? defaultIconClass : 'hidden'
-  const sunMoonIconClass = theme === Theme.System ? defaultIconClass : 'hidden'
-
   return (
     <Button variant="ghost" size="icon" onClick={toggleTheme}>
-      <Sun className={sunIconClass} />
-      <Moon className={moonIconClass} />
-      <SunMoon className={sunMoonIconClass} />
+      {theme === Theme.Light
+        ? (
+            <Sun className={styleIcon} />
+          )
+        : (
+            <Moon className={styleIcon} />
+          )}
     </Button>
   )
 }
