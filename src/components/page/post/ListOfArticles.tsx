9c20c73e7178c274560a6cecbe745e620a70a04a
QuentinHsu <xuquentinyang@gmail.com>
2024-07-14 20:02:52 +0800

    refactor: improve code readability and performance in ListOfArticles component

--- src/components/page/post/ListOfArticles.tsx
+++ src/components/page/post/ListOfArticles.tsx
@@ -33,14 +33,18 @@ const PostList: React.FC<PostListProps> = ({ posts }) => {
   return (
     <ul>
       {Array.from(postsByYear).map(([year, posts]) => (
-        <li key={year}>
-          <h2 className="text-2xl font-bold">{year}</h2>
-          <ul className="ml-4">
+        <li key={year} className="py-10 mt-20">
+          <h2 className="md:text-9xl text-7xl font-bold stroke-text absolute md:mt-[-4rem] md:ml-[-3rem] mt-[-1.5rem] ml-[-0.5rem]">
+            {year}
+          </h2>
+          <ul className="ml-4 relative">
             {posts.map(post => (
               <li key={post.slug}>
-                <a href={`/blog/${post.slug}/`} className="flex items-center justify-between">
-                  <h4 className="md:text-xl">{post.data.title}</h4>
-                  <p className="date m-2">{dayjs(post.data.createdTime).format('MM-DD')}</p>
+                <a href={`/blog/${post.slug}/`} className="w-full flex items-center justify-between dark:text-gray-500 dark:hover:text-gray-300 text-slate-500 hover:text-slate-950  duration-300">
+                  <div className="md:text-xl">
+                    {post.data.title}
+                  </div>
+                  <p className="m-2">{dayjs(post.data.createdTime).format('MM-DD')}</p>
                 </a>
               </li>
             ))}
