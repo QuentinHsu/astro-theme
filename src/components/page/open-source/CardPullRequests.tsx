a2ff05e1b6c7e4481f4549d74579242bc798c6d3
QuentinHsu <xuquentinyang@gmail.com>
2024-07-14 10:40:23 +0800

    feat: PullRequests

--- src/components/page/open-source/CardPullRequests.tsx
+++ src/components/page/open-source/CardPullRequests.tsx
@@ -1,25 +1,39 @@
 import { z } from 'astro/zod'
 import React, { useEffect, useState } from 'react'
+import { GitHubLogoIcon } from '@radix-ui/react-icons'
+import dayjs from 'dayjs'
 
 const SProps = z.object({
+  className: z.string().optional(),
   repository: z.string().url(),
   author: z.string(),
   pageSize: z.number().default(10).optional(),
 })
 const SPullRequest = SProps.extend({})
+const SPullRequestItem = z.object({
+  title: z.string(),
+  url: z.string().url(),
+  number: z.number(),
+  mergedAt: z.string().optional(),
+
+})
 
 type IProps = z.infer<typeof SProps>
 type IPullRequest = z.infer<typeof SPullRequest>
+type IPullRequestItem = z.infer<typeof SPullRequestItem>
 
-interface PullRequest {
-  title: string
-  url: string
-}
-
-const CardPullRequests: React.FC<IProps> = ({ repository, author, pageSize = 10 }) => {
+const CardPullRequests: React.FC<IProps> = ({ repository, author, pageSize = 10, className }) => {
+  const repositoryPath = repository.split('/').slice(-2).join('/')
   return (
-    <div>
-      <div>{repository}</div>
+    <div className={className}>
+      <div className="flex items-center space-x-2">
+        <h3 className="text-xl">
+          {repositoryPath}
+        </h3>
+        <a href={repository} target="_blank" rel="noreferrer">
+          <GitHubLogoIcon />
+        </a>
+      </div>
       <ListPullRequests repository={repository} author={author} pageSize={pageSize} />
     </div>
   )
@@ -29,7 +43,7 @@ export default CardPullRequests
 
 function ListPullRequests(props: IPullRequest): React.ReactElement {
   const [loading, setLoading] = useState<boolean>(true)
-  const [pullRequests, setPullRequests] = useState<PullRequest[]>([])
+  const [pullRequests, setPullRequests] = useState<IPullRequestItem[]>([])
   const [error, setError] = useState<string | null>(null)
   useEffect(() => {
     const repositoryPath = props.repository.split('/').slice(-2).join('/')
@@ -46,6 +60,8 @@ function ListPullRequests(props: IPullRequest): React.ReactElement {
         const newPullRequests = data.items.map((item: any) => ({
           title: item.title,
           url: item.html_url,
+          number: item.number,
+          mergedAt: item?.pull_request?.merged_at,
         }))
         setPullRequests(newPullRequests)
       }
@@ -69,10 +85,29 @@ function ListPullRequests(props: IPullRequest): React.ReactElement {
         </div>
       )}
       {pullRequests.map(pullRequest => (
-        <div key={pullRequest.url}>
+        <div key={pullRequest.url} className="p-2">
           <a href={pullRequest.url} target="_blank" rel="noreferrer">{pullRequest.title}</a>
+          {' '}
+
+          <div className="flex space-x-2 text-xs text-slate-500">
+            <div>
+              #
+              {pullRequest.number}
+            </div>
+            <span className="">
+              Merged on
+              {' '}
+              {dayjs(pullRequest.mergedAt).format('YYYY-MM-DD')}
+            </span>
+          </div>
+
         </div>
       ))}
+      <div className="ml-2">
+        count:
+        {' '}
+        {pullRequests.length}
+      </div>
     </div>
   )
 }
